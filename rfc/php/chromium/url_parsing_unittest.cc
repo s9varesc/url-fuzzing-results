a56090eaa99ebb2352848117c83b65b515bb0d0b
root <root@vagrant.vm>
2021-07-08 10:41:19 +0000

    update results

--- rfc/php/chromium/url_parsing_unittest.cc
+++ rfc/php/chromium/url_parsing_unittest.cc
@@ -32,7 +32,15 @@ static URLParseCase parse_cases[]={{"//[fbdd:bbfb:bdfe:cafd:efbe:cddd:2aae:fddc%
 {"H://[::dabf:edad:eabc:cbef:aacf:4bbb:afba]"},
 {"//[adcf:dffc:ebfa:cbbc:beea:afcc:62.251.255.255]"},
 {"///@/%1d"},
-{"p://*"}};
+{"p://*"},
+{"//[::abab:afdf:efbe:ddda:fcac:aadb:afea%25-]"},
+{"//[6bef::bbbc:255.252.251.255]"},
+{"///@/%e7"},
+{"I://[::250.253.200.252%25.]"},
+{"W://[fcbb:dcef:ddab:bfba:cdbb:beef:252.254.77.253%25-]"},
+{"//[::aded:cffa:dcde:252.255.224.254%25_]"},
+{"//[adcc::ceda:ddfb:bdeb:efbf:252.252.232.250]"},
+{"//%d2"}};
 //test execution
 TEST(URLParser, Parsing){
 	for (size_t i = 0; i < base::size(parse_cases); i++) {
